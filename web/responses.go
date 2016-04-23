04a82b39a8bf1bcac34d5c1603c347efe36d34d2
Mohammad Nasirifar <m.nasirifar@live.com>
2016-04-23 19:02:00 +0430

    Bug fix in yaml serialization of responses

--- web/responses.go
+++ web/responses.go
@@ -3,7 +3,7 @@ package web
 type CountResponse struct {
 	Hostname              string `yaml:"hostname"`
 	RequestedPath         string `yaml:"requestedPath"`
-	TotalNumberOfRequests int    `yaml:totalNumberOfRequests"`
+	TotalNumberOfRequests int    `yaml:"totalNumberOfRequests"`
 	Count                 int    `yaml:"count"`
 	CurrentTimeUnixNano   int64  `yaml:"currentTimeUnixNano"`
 	UptimeUnixNano        int64  `yaml:"uptimeUnixTime"`
@@ -13,7 +13,7 @@ type CountResponse struct {
 type CountResetResponse struct {
 	Hostname              string `yaml:"hostname"`
 	RequestedPath         string `yaml:"requestedPath"`
-	TotalNumberOfRequests int    `yaml:totalNumberOfRequests"`
+	TotalNumberOfRequests int    `yaml:"totalNumberOfRequests"`
 	Count                 int    `yaml:"count"`
 	SetTo                 int    `yaml:"setTo"`
 	CurrentTimeUnixNano   int64  `yaml:"currentTimeUnixNano"`
@@ -25,7 +25,7 @@ type InfoResponse struct {
 	Hostname              string `yaml:"hostname"`
 	RequestedPath         string `yaml:"requestPath"`
 	RemoteAddress         string `yaml:"remoteAddress"`
-	TotalNumberOfRequests int    `yaml:totalNumberOfRequests"`
+	TotalNumberOfRequests int    `yaml:"totalNumberOfRequests"`
 	CurrentTimeUnixNano   int64  `yaml:"currentTimeUnixNano"`
 	UptimeUnixNano        int64  `yaml:"uptimeUnixTime"`
 	UptimeHumanReadable   string `yaml:"uptimeHumanReadable"`
